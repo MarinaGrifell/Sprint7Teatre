9a5d2beef6fcdfd5b5db8b2aa8f6d68a32bdd919
MarinaGrifell <marinagrifell@hotmail.com>
2021-05-02 21:39:16 +0200

    ejercicio1-ejercicio2-ejercico3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import Linia from './linia'
-import linies from './linies.json'
+import React,{useState} from 'react';
+import Linia from './components/linia'
+import linies from './data/linies.json'
 import styled from 'styled-components'
+
 
 const StyledLinia= styled.h1` 
 background: pink;
 color: grey;
 font-size: 15px;
-text-align: center;
 `;
 
 function App() {
+const [count,setCount]=useState(0);
+  function increment(){
+    if ( count < linies.length-1) { setCount(count + 1)}
+    console.log(count);
+  }
+
+  function decrement() {
+    if ( count > 0) {setCount(count - 1) }
+    console.log(count);
+
+  }
+  const liniaSelected =  linies.slice(count,count+1).map((item,index)=> {
+    return (
+    
+    <StyledLinia key={index}>
+    <Linia title={item}></Linia>
+     </StyledLinia>
+    
+    )
+   
+  })
+
   return (
     <div className="App">
-    {linies.map((item)=> {
-      return (
-      
-      <StyledLinia>
-      <Linia title={item}></Linia>
-       </StyledLinia>
-      
-      )
-     
-    })}
+
+
+    <button onClick={decrement}>Anterior</button>
+    <button onClick={increment}>Següent</button>
+   
+    {
+      liniaSelected
+     }
     
      
     </div>
